77439e1a2105bb07be9656e0810a6c5744c6b8c6
srimalonline <dilankasrimal23@gmail.com>
2023-03-27 08:41:03 +0530

    commenting search function

--- frontend/src/app/components/teacher-table/teacher-table.component.ts
+++ frontend/src/app/components/teacher-table/teacher-table.component.ts
@@ -60,19 +60,19 @@ export class TeacherTableComponent implements OnInit {
     })
   }
 
-  search(value) {
-    let foundItems = [];
-    if (value.length <= 0) {
-      this.getTeacherData();
-    } else {
-      let b = this.teacherData.filter((teacher) => {
-        if (teacher[0].name.toLowerCase().indexOf(value) > -1) {
-          foundItems.push(teacher)
-        }
-      });
-      this.teacherData = foundItems;
-    }
-  }
+  // search(value) {
+  //   let foundItems = [];
+  //   if (value.length <= 0) {
+  //     this.getTeacherData();
+  //   } else {
+  //     let b = this.teacherData.filter((teacher) => {
+  //       if (teacher[0].name.toLowerCase().indexOf(value) > -1) {
+  //         foundItems.push(teacher)
+  //       }
+  //     });
+  //     this.teacherData = foundItems;
+  //   }
+  // }
 
   deleteTeacher(itemid) {
     const test = {
